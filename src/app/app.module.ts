52a3dc499836507c72e7d55d5b0064302a4f8672
nfalceso <npafalceso@gmail.com>
2019-06-02 21:57:30 +0800

    06/02/2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+import { PokemonSearchComponent } from './pokemon-search/pokemon-search.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonDetailsComponent,
+    PokemonSearchComponent,
+    PokemonComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
