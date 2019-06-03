11d06138cf162ed99d7bb9f8ef40a13054be6fae
Neil Patrick Angelo V. Falceso <nfalceso@SVI.COM.PH>
2019-06-03 05:30:24 -0400

    06/03/2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.compo
 import { PokemonSearchComponent } from './pokemon-search/pokemon-search.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { HeaderComponent } from './header/header.component';
 import { BerriesComponent } from './berries/berries.component';
 
