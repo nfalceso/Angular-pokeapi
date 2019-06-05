0353289c1c003e29551560bed3ede893d0ee1f98
nfalceso <npafalceso@gmail.com>
2019-06-05 19:32:15 +0800

    add sidebar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MdMenuModule, MdButtonModule, MdIconModule, MdCardModule } from '@angular/material'
+
 import { AppComponent } from './app.component';
 import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 import { PokemonSearchComponent } from './pokemon-search/pokemon-search.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { HeaderComponent } from './header/header.component';
 import { BerriesComponent } from './berries/berries.component';
@@ -16,13 +18,17 @@ import { BerriesComponent } from './berries/berries.component';
     PokemonDetailsComponent,
     PokemonSearchComponent,
     PokemonComponent,
-    DashboardComponent,
+    // DashboardComponent,
     PokemonListComponent,
     HeaderComponent,
     BerriesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdMenuModule,
+    MdButtonModule,
+    MdIconModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
